43a9945e8cd2e4fe6fe0ec2f1cc8b8758950cdca
Teodora Tsvetanov <teodora.tsvetanov@gmail.com>
2021-06-30 18:47:44 -0500

    more changes to US100

--- src/test/java/UserStory100.java
+++ src/test/java/UserStory100.java
@@ -1,6 +1,7 @@
 public class UserStory100 {
     public static void main(String[] args) {
         System.out.println("Today we are having a Git class");
+        System.out.println("work 1");
     }
 
 }
