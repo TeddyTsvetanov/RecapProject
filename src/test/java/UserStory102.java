b3896ec77072f1cc976d5e3a297f8326017e3a28
Teodora Tsvetanov <82928114+TeddyTsvetanov@users.noreply.github.com>
2021-06-30 20:27:53 -0500

    Us102 has been completed (#2)
    Combine all 4 commits by squashing it

---
+++ src/test/java/UserStory102.java
@@ -0,0 +1,10 @@
+public class UserStory102 {
+    //work 1
+    //work 2
+    //lots of code has been written here
+    //ready to push this branch to remote
+    //and open pull request so I can start collaborating
+    //so my teammember can comment review my code and approve or request for more changes
+
+
+}
